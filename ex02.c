275cb904da02e0c9f504a2013378f5edac0585d2
Gabriel Leonel Santos da Silva <gabriel.leonel2016@gmail.com>
2024-12-03 22:08:16 -0300

    Update ex02.c

--- ex02.c
+++ ex02.c
@@ -1,5 +1,7 @@
 // 2. Concatenando Strings : Leia duas strings e concatene - as, imprimindo o resultado.Crie uma função para concatenar duas strings.
 
+// BUG solucionado: Agora imprime as duas strings separadas utilizano o método fgets ao invés do scanf
+
 #include <stdio.h>
 #include <string.h>
 
@@ -13,8 +15,8 @@ char* concatenaString(char str1[], char str2[]){
 int main(){
     char str1[30], str2[30];
     printf("Digite a primeira string: ");
-    scanf("%s", str1);
+    fgets(str1, 30, stdin);
     printf("Digite a segunda string: ");
-    scanf("%s", str2);
+    fgets(str2, 30, stdin);
     printf("Strings concatenadas: %s", concatenaString(str1, str2));
 }
